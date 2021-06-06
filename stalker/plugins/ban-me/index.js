873dea23393279aaa049a0aad45ac0911bc6dda5
edfus <edfusedfus@gmail.com>
2021-06-06 19:04:47 +0800

    plugin: implement ban-me

--- stalker/plugins/ban-me/index.js
+++ stalker/plugins/ban-me/index.js
@@ -1,27 +1,114 @@
-const banPattern = /^ban(\s|(?=me))/i;
-const targerPattern = /^(me|ME)/;
+import parseDuration from "parse-duration";
+import RandExp from "randexp";
 
-export default function (ctx, next) {
-  if(ctx.from !== "group") {
+// https://en.wikipedia.org/wiki/Letterlike_Symbols
+const emoji = new RandExp(/👎|👀|💩|👊/);
+const penis = new RandExp(/[ℙ][ℇ℮ℯℰⅇ][ℕ№][iℹ℩ⅈ]s|[ⅅⅆ⅁][ℹ️ℹ℩ⅈ][ℂ℃℄]K/i);
+penis.defaultRange.add(0, 65535);
+
+const exclamation = new RandExp(/wow|lol|lmao|\?/i);
+const emphasizeAdv = new RandExp(/(pathetically|inordinately)?/);
+const sadVerb = new RandExp(/hurt|feel sad|feel such pain/);
+const small = new RandExp(/small|tiny/);
+const too = new RandExp(/to{2,5}/);
+const parts = [
+  { 
+    start: () => "Isn't your",
+    end:   () =>`${emphasizeAdv.gen()} ${too.gen()} small?` 
+  },
+  { 
+    start: () =>`${exclamation.gen()}, is that the size of your`,
+    end:   () =>`?` 
+  },
+  { 
+    start: () => `I ${sadVerb.gen()} for your ${small.gen()}`,
+    end:   () =>`size` 
+  }
+];
+
+const insult = (p, e) => {
+  const part = parts[Math.floor(Math.random() * parts.length)];
+  return `${part.start()} ${p} ${part.end()} ${e}`;
+};
+
+let i = 0;
+const banPattern = /^ban(\s|(?=me))/i;
+const targetPattern = /^(me|ME)/;
+
+export const command = "/ban (me|@yourself) [ duration (in English) ]/i";
+
+export default async function (ctx, next) {
+  if(ctx.from !== "group" || !ctx["@me"]) {
     return next();
   }
 
-  if(banPattern.test(ctx.state.command)) {
+  const command = [ ...ctx.command ];
+
+  if(command[0].type === "text" && banPattern.test(command[0].value)) {
     try {
-      const commandContent = ctx.state.command.replace(banPattern, "").trim();
-      if(!targerPattern.test(commandContent)) {
-        return ctx.respond(ctx.getReaction("reject"));
+      const textTarget = command[0].value.replace(banPattern, "").trim();
+
+      const groupID = ctx.groupID;
+      const senderID = ctx.senderID;
+
+      if(!targetPattern.test(textTarget)) {
+        if(command[1]?.type === "at" && Number(command[1].value) == senderID) {
+          ;
+        } else { 
+          return ctx.respond("Go fuck yourself");
+        }
       }
 
-      const group_id = ctx.data.group_id;
-      const user_id = ctx.data.user_id;
-      const duration = parseInt(commandContent.replace(targerPattern, ""));
+      let durationInSeconds = 120;
+      const durationSources = (
+        targetPattern.test(textTarget)
+          ? [ 
+              {
+                type: "text",
+                value: textTarget.replace(targetPattern, "")
+              },
+              ...command.slice(1)
+            ]
+          : command.slice(2)
+      );
 
-      return ctx.bot.setGroupBan(group_id, user_id, duration || 120);
+      const digitsOnly = /^\s*\d+\.?\d+\s*$/;
+
+      for (const commandNode of durationSources) {
+        if(commandNode?.type === "text") {
+          if(digitsOnly.test(commandNode.value)) {
+            durationInSeconds = parseInt(parseDuration(
+              commandNode.value.concat("s")
+            ));
+            break;
+          }
+          const duration = parseInt(parseDuration(commandNode.value));
+          if(duration) {
+            durationInSeconds = (duration / 1000).toFixed(0);
+            break;
+          }
+        }
+      }
+
+      if(durationInSeconds < 2) {
+        const p = penis.gen();
+        const e = emoji.gen();
+        return ctx.respond(insult(p, e));
+      }
+
+      try {
+        await ctx.bot.setGroupBan(groupID, senderID, durationInSeconds);
+      } catch (err) {
+        return ctx.respond("Can a sheep beat a wolf?"); //NOTE
+      }
+
+      if(durationInSeconds > 300) {
+        return ctx.respond(`Based. [${++i}] ${durationInSeconds}s`);
+      } else {
+        return ctx.respond(ctx.getReaction("accept"));
+      }
     } catch (err) {
-      err.expose = true;
-      err.status = ctx.state.command;
-      return ctx.throw(err);
+      return ctx.throw(err, ctx.commandText);
     }
   }
 
