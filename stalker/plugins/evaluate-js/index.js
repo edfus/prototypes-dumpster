6964e762eabd80d794999c4d39ff2cb2afa51b2b
edfus <edfusedfus@gmail.com>
2021-06-06 21:20:06 +0800

    plugin: enhance evaluate-js

--- stalker/plugins/evaluate-js/index.js
+++ stalker/plugins/evaluate-js/index.js
@@ -1,31 +1,33 @@
 import { VM } from 'vm2';
 const vm = new VM();
 
+import { inspect } from "util";
+
 export const priority = 5;
 export const command = "expr( 1 + 1 ) [ can have multiple lines ]";
+
+const commandPattern = /^\s*expr(?=[\s('"`{\[+\-!^])/i;
 
 export default async function (ctx, next) {
   if(ctx.from === "group" && !ctx["@me"]) {
     return next();
   }
 
-  if(/^\s*expr\s|\(/i.test(ctx.commandText)) {
-    const toRun = ctx.commandText.replace(/^\s*expr\s?/i, "");
+  if(commandPattern.test(ctx.commandText)) {
+    const toRun = ctx.commandText.replace(commandPattern, "");
 
-    // if (ctx.environment !== "production") {
-      if(toRun.length > 40) {
-        return ctx.respond(ctx.getReaction("reject"));
-      }
-    // }
+    if(toRun.length > 80) {
+      return ctx.respond(ctx.getReaction("reject"));
+    }
     
     try {
       const result = await vm.run(toRun);
       if(result instanceof Error) {
         result.stack = "***";
       }
-      return ctx.respond(result);
+      return ctx.respond(inspect(result || ""));
     } catch (err) {
-      return ctx.throw(err, toRun);
+      return ctx.throw(err, "Errored, that's it.");
     }
   }
 
