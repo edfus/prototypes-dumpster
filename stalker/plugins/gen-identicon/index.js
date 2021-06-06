b2f28b777f2f68d11e6bf36312f32eb5f803cd53
edfus <edfusedfus@gmail.com>
2021-06-06 15:34:23 +0800

    plugin: implement gen-identicon

---
+++ stalker/plugins/gen-identicon/index.js
@@ -0,0 +1,37 @@
+import Identicon from "identicon.js";
+import oicq from "oicq";
+
+import { createHash } from "crypto";
+
+const commandPattern = /^\s*(gen(erate)?\s+)?(avatar|identicon|icon)(\s+|\(|$)/;
+export const command = commandPattern.source.concat(" input");
+
+export default function (ctx, next) {
+  if(ctx.from === "group" && !ctx.isAtMe) {
+    return next();
+  }
+
+  if(commandPattern.test(ctx.commandText)) {
+    if(ctx.bot.canSendImage().status !== "ok") {
+      return ctx.respond("Tencent image upload sucks");
+    }
+
+    return ctx.sendMedia(
+      oicq.segment.image(
+        "base64://".concat(
+          new Identicon(
+            createHash("sha1").update(
+              ctx.commandText.replace(commandPattern, "") || String(Math.random())
+            ).digest("hex"), 
+            {
+              size: 200,
+              format: "png"
+            }
+          ).toString()
+        )
+      )
+    );
+  }
+
+  return next();
+}
