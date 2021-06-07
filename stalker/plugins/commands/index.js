d94d6490a148ec7188ed10e5dbefd6488d9443b8
edfus <edfusedfus@gmail.com>
2021-06-07 23:29:53 +0800

    plugin: enlarge plugin commands's matching range

--- stalker/plugins/commands/index.js
+++ stalker/plugins/commands/index.js
@@ -1,4 +1,4 @@
-const commandPattern = /^commands|help|plugins?$/i;
+const commandPattern = /^commands?|h(elp)?|plugins?$/i;
 export const command = commandPattern.source;
 
 const newline = "\r\n";
