7ba9437bd7667b912e1307c2e4998433e93823e0
edfus <edfusedfus@gmail.com>
2021-06-07 23:44:06 +0800

    plugin: implement title-me

--- stalker/plugins/title-me/index.js
+++ stalker/plugins/title-me/index.js
@@ -1,1 +1,26 @@
-// async client.setGroupSpecialTitle(group_id, user_id[, special_title, duration])
+const commandPattern = /^title\s*me\s+/i;
+
+export const command = commandPattern.source.concat("< new title >");
+
+export default async function (ctx, next) {
+  if(ctx.from !== "group" || !ctx["@me"]) {
+    return next();
+  }
+
+  if(commandPattern.test(ctx.commandText)) {
+    const title = ctx.commandText.replace(commandPattern, "");
+    if(!title || title.length > 40) {
+      return ctx.respond("pardon?");
+    }
+
+    await ctx.respond(ctx.getReaction("accept"));
+    try {
+      await ctx.bot.setGroupSpecialTitle(ctx.groupID, ctx.senderID, title);
+    } catch (err) {
+      return ctx.respond("but can't");
+    }
+    return ;
+  }
+
+  return next();
+}
