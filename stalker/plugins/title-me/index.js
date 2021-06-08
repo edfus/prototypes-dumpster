94784b0fb920ea4ef9bfb5e3346a6d88d1a0787e
edfus <edfusedfus@gmail.com>
2021-06-08 12:48:09 +0800

    enhance(plugin): title-me

--- stalker/plugins/title-me/index.js
+++ stalker/plugins/title-me/index.js
@@ -2,6 +2,8 @@ const commandPattern = /^title\s*me\s+/i;
 
 export const command = commandPattern.source.concat("< new title >");
 
+const trollers = new Map();
+
 export default async function (ctx, next) {
   if(ctx.from !== "group" || !ctx["@me"]) {
     return next();
@@ -9,13 +11,58 @@ export default async function (ctx, next) {
 
   if(commandPattern.test(ctx.commandText)) {
     const title = ctx.commandText.replace(commandPattern, "");
-    if(!title || title.length > 40) {
+    if(!title || title.length > 30) {
       return ctx.respond("pardon?");
+    }
+
+    switch (ctx.sender.role) {
+      case "owner":
+        if(trollers.has(ctx.senderID)) {
+          const trollHistory = trollers.get(ctx.senderID);
+          const lastTroll = trollHistory.last;
+          const dateNow = Date.now();
+
+          trollHistory.last = dateNow;
+
+          if(trollHistory.blocked) {
+            return ctx.respond(ctx.getReaction("fooled"));
+          }
+
+          if(dateNow - lastTroll > 1000 * 60 * 60 * 24 * 10) {
+            await ctx.respond("?... i'll give it a go anyway");
+            try {
+              await ctx.bot.setGroupCard(ctx.groupID, ctx.senderID, title);
+              return trollHistory.delete(ctx.senderID);
+            } catch (err) {
+              trollHistory.times++;
+              trollHistory.blocked = true;
+              return ctx.respond("...and failed miserably");
+            }
+          }
+
+          switch (trollHistory.times++) {
+            case 1:
+              return ctx.respond("ugh i cant bro");
+            case 2:
+              return ctx.respond("...");
+            default:
+              return ctx.respond(ctx.getReaction("fooled"));
+          }
+        } else {
+          trollers.set(ctx.senderID, {
+            times: 1,
+            last: Date.now(),
+            blocked: false
+          });
+        }
+        return ctx.respond("i cant");
+      default:
+        break;
     }
 
     await ctx.respond(ctx.getReaction("accept"));
     try {
-      await ctx.bot.setGroupSpecialTitle(ctx.groupID, ctx.senderID, title);
+      await ctx.bot.setGroupCard(ctx.groupID, ctx.senderID, title);
     } catch (err) {
       return ctx.respond("but can't");
     }
