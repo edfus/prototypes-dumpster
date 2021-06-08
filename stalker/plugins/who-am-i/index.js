7124049e5e66823ee8d752aa10ed921a05994aeb
edfus <edfusedfus@gmail.com>
2021-06-08 12:48:21 +0800

    enhance(plugin): who-am-i

--- stalker/plugins/who-am-i/index.js
+++ stalker/plugins/who-am-i/index.js
@@ -1,4 +1,4 @@
-const commandPattern = /^who\s*(am|is)\s*i\s*$/i;
+const commandPattern = /^who\s*am\s*i\s*$/i;
 
 export const command = commandPattern.source;
 
@@ -8,21 +8,22 @@ export default async function (ctx, next) {
   }
 
   if(commandPattern.test(ctx.commandText)) {
-    await ctx.respond([
-      ctx.sender.nickname,
-      ctx.sender.title && `the ${ctx.sender.title}`,
+    const title = ctx.sender.title || ctx.sender.card;
+
+    return ctx.respond([
+      ctx.sender.nickname.concat?.(title ? ` the ${title}` : ""),
+      ctx.sender.sex !== "unknown" && ctx.sender.sex,
       ctx.sender.age && `${ctx.sender.age} years old`,
-      ctx.sender.sex,
       ctx.sender.area !== "unknown" && `living in ${ctx.sender.area}`,
       ctx.sender.level && `with a level of ${"💩".repeat(parseInt(ctx.sender.level))}`,
-      ctx.sender.role && `and is the ${ctx.sender.role}`
+      ["admin", "owner"].includes(ctx.sender.role)
+      ? `and is the ${ctx.sender.role}`
+      : [
+        "pooping three times a day",
+        "suffering from ureter stones",
+        "jerking off every four hours"
+      ][Math.floor(Math.random() * 3)]
     ].filter(Boolean).join(", ").concat("."), false);
-
-    if(ctx.sender.card) {
-      return ctx.respond(ctx.sender.card, false);
-    } else {
-      return ;
-    }
   }
 
   return next();
