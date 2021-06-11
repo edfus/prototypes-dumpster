8afb6ca96678a3ddc5a3d3a6382fa32018fab94d
edfus <edfusedfus@gmail.com>
2021-06-11 14:42:53 +0800

    refactor: to a service behind proxies

--- stalker/index.js
+++ stalker/index.js
@@ -6,11 +6,12 @@ import { InputAgent } from "./src/input-agent.js";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
-import Koa from "koa";
 import RateLimiter from "async-ratelimiter";
 import Redis from "ioredis";
+
 import { inspect } from "util";
-import { Authenticator, generateTokens } from "./auth-server.js";
+import { createServer } from "http";
+import { pipeline, Writable } from "stream";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const pluginsPath = join(__dirname, "./plugins");
@@ -23,9 +24,14 @@ const env = {
   STALKER_CREDENTIALS_PATH: process.env["STALKER_CREDENTIALS_PATH"],
   STALKER_NOTIFY_PORT: process.env["STALKER_NOTIFY_PORT"],
   STALKER_SET_BASIC_INFO: process.env["STALKER_SET_BASIC_INFO"],
+  REDIS_PORT: process.env["REDIS_PORT"],
+  REDIS_HOSTNAME: process.env["REDIS_HOSTNAME"]
 };
 
-const setBasicInfo = env["STALKER_SET_BASIC_INFO"] === "true";
+let setBasicInfo = (
+  env["STALKER_SET_BASIC_INFO"] === undefined
+  ? true : ["true", "0"].includes(env["STALKER_SET_BASIC_INFO"])
+);
 
 ;(async () => {
   const app = new App();
@@ -62,22 +68,26 @@ const setBasicInfo = env["STALKER_SET_BASIC_INFO"] === "true";
     if(master) {
       return bot.sendPrivateMsg(master, "[meta] ".concat(message));
     }
+  };
+
+  const setStatus = async () => {
+    await bot.setOnlineStatus(70); // do not disturb
+    if(setBasicInfo) {
+      basicInfo.nickname && await bot.setNickname(basicInfo.nickname);
+      basicInfo.avatar && await bot.setPortrait(basicInfo.avatar);
+      basicInfo.signature && await bot.setSignature(basicInfo.signature);
+    }
   }
 
   let retriedCount = 0, lastReport = 0;
   bot.on("system.online", async () => {
-    await bot.setOnlineStatus(70); // do not disturb
-    if(setBasicInfo) {
-      await bot.setNickname(basicInfo.nickname);
-      await bot.setPortrait(basicInfo.avatar);
-      await bot.setSignature(basicInfo.signature);
-    }
+    await setStatus();
+
     if(lastReport < retriedCount) {
       await notifyMaster(
-        "[system] just recovered from a panic attack,"
-      );
-      await notifyMaster(
-        `[system] after system retried ${retriedCount - lastReport} times.`
+        `[system] just recovered from a panic attack, which took ${
+          retriedCount - lastReport
+        } retries.`
       );
       lastReport = retriedCount;
     }
@@ -113,7 +123,155 @@ const setBasicInfo = env["STALKER_SET_BASIC_INFO"] === "true";
     }
   });
 
-  app.on("respond", async info => {
+  if(env["REDIS_PORT"]) {  
+    const redis  = new Redis(Number(env["REDIS_PORT"]), env["REDIS_HOSTNAME"] || "localhost");
+    
+    const limitDuration = 60000; // 1 minute
+    const rateLimiter = new RateLimiter({
+      db: redis,
+      max: 60,
+      duration: limitDuration,
+      namespace: "limit-bot-messaging"
+    });
+    
+    const globalLimit = {
+      totoal: 120,
+      remaining: 120,
+      duration: 80000,
+      reported: false,
+      lastReset: Date.now()
+    };
+
+    const rateLimited = new Map();
+    
+    setInterval(() => {
+      const now = Date.now();
+      if(now - globalLimit.lastReset >= globalLimit.duration) {
+        globalLimit.remaining = globalLimit.totoal;
+        globalLimit.reported  = false;
+        globalLimit.lastReset = now;
+      }
+      
+      for (const [id, lastReset] of rateLimited.entries()) {
+        if(now - lastReset >= limitDuration) {
+          rateLimited.delete(id);
+        }
+      }
+    }, limitDuration / 2).unref();
+
+    app.on("respond", async (ctx, info) => {
+      if(--globalLimit.remaining < 0) {
+        globalLimit.remaining = 0;
+      }
+      ctx.from === "private"
+        ? await rateLimiter.get({ id: ctx.senderID, max: 20, decrease: true })
+        : await rateLimiter.get({ id: ctx.groupID, decrease: true })
+      ;
+    });
+
+    app.prepend({
+      meta: {
+        name: "rate-limit"
+      },
+      middleware: async (ctx, next) => {
+        if(!globalLimit.remaining) {
+          if(globalLimit.reported) {
+            return ;
+          } else {
+            await notifyMaster(
+              `[warn] global ${globalLimit.totoal} rate-limit reached.`
+            );
+            globalLimit.reported = true;
+            await ctx.bot.setOnlineStatus(31); // away
+          }
+        }
+
+        const id = ctx.from === "private" ? ctx.senderID : ctx.groupID;
+        if(rateLimited.has(id)) {
+          return ;
+        }
+
+        const limit = (
+          ctx.from === "private"
+            ? await rateLimiter.get({ id: id, max: 20, decrease: false })
+            : await rateLimiter.get({ id: id, decrease: false })
+        );
+
+        if(!limit.remaining) {
+          rateLimited.set(id, Date.now());
+          await notifyMaster(
+            `[warn] rate-limit reached for ${ctx.from}-${id}-${
+              ctx.groupName || ctx.sender.nickname
+            }`
+          );
+          return ctx.respond("Ratelimited, staying low for a while, cya");
+        }
+
+        return next();
+      }
+    });
+  }
+
+  if(env["STALKER_NOTIFY_PORT"]) {
+    createServer(async (req, res) => {
+      if(req.method !== "PUT") {
+        return res.writeHead(405).end();
+      }
+      
+      const toSend = [];
+      try {
+        let length = 0;
+        const textDecoder = new TextDecoder();
+        await new Promise((resolve, reject) => {
+          pipeline(
+            req,
+            new Writable({
+              write(chunk, enc, cb) {
+                const part = textDecoder.decode(chunk, { stream: true });
+                length += part.length;
+                if(length > 500) {
+                  return cb(new Error("Length too long"));
+                }
+                toSend.push(part);
+                return cb();
+              },
+              final (cb) {
+                const lastPart = textDecoder.decode();
+                toSend.push(lastPart);
+                return cb();
+              }
+            }),
+            err => err ? reject(err) : resolve()
+          )
+        });
+
+        if(master) {
+          const message = toSend.join("");
+          await bot.sendPrivateMsg(master, "[notify] ".concat(message));;
+          await inputAgent.info(
+            [
+              `[${new Date().toLocaleString()}] Notified master about '${
+                message.length > 50 ? message.slice(0, 50).concat("...") : message
+              }'.`
+            ],
+            "yellow"
+          );
+        } else {
+          throw new Error("An orphan, I am");
+        }
+      } catch (err) {
+        return res.writeHead(400).end(err.message);
+      }
+      return res.writeHead(200).end("ok");
+    }).listen(Number(env["STALKER_NOTIFY_PORT"]), () => {
+      inputAgent.info(
+        `Server is running at http://0.0.0.0:${env["STALKER_NOTIFY_PORT"]}`,
+        "green"
+      );
+    });
+  }
+
+  app.on("respond", async (ctx, info) => {
     if(info.msg.length >= 40) {
       info.msg = info.msg.slice(0, 40).concat("...");
     }
@@ -147,118 +305,39 @@ const setBasicInfo = env["STALKER_SET_BASIC_INFO"] === "true";
     await notifyMaster(`[error] ${inspect(err)}`);
   });
 
-  let callback = app.callback(bot, await loadPlugins(pluginsPath));
+  let callback;
+  const commandIdentified = "!command ";
+  app.prepend(
+    {
+      meta: {
+        name: "commands-4-master"
+      },
+      middleware: async (ctx, next) => {
+        if(ctx.senderID === master && ctx.from === "private") {
+          const command = ctx.commandText.trim();
+          if(command.startsWith(commandIdentified)) {
+            const cmd = command.slice(commandIdentified.length);
+            if(/^[Rr]eload$/.test(cmd)) {
+              await inputAgent.prompt("Reloading plugins as required by master...");
+              bot.removeListener("message", callback);
+        
+              callback = app.callback(bot, await loadPlugins(pluginsPath, true));
+              bot.on("message", callback); 
+              return ctx.respond("[meta] reloaded");
+            }
+          }
+        }
+    
+        return next();
+      }
+    }
+  );
+
+  callback = app.callback(bot, await loadPlugins(pluginsPath));
 
   bot.on("message", callback)
      .login(credentials.password_md5)
   ;
-
-  if(env["STALKER_NOTIFY_PORT"]) {
-    const webApp = new Koa({ proxy: true, maxIpsCount: 1 });
-    const redis  = new Redis(env["REDIS_PORT"], env["REDIS_HOSTNAME"]);
-    const rateLimiter = new RateLimiter({
-      db: redis,
-      max: 80,
-      duration: 60000, // 1 minute
-      namespace: "limit-api"
-    });
-
-    const origin = env["STALKER_DOMAIN_ORIGIN"];
-
-    webApp.context.format = ctx => {
-      return `<${
-        new Date().toLocaleString()
-      }> ${ctx.ip} ${ctx.method} ${ctx.url} ${ctx.status}`
-    };
-
-    webApp.use(async (ctx, next) => {
-      const ip = ctx.ip;
-      const limit = await rateLimiter.get({ id: ip })
-
-      if (!ctx.response.headerSent) {
-        ctx.response.set('X-Rate-Limit-Limit', limit.total);
-        ctx.response.set('X-Rate-Limit-Remaining', Math.max(0, limit.remaining - 1));
-        ctx.response.set('X-Rate-Limit-Reset', limit.reset);
-      }
-
-      if(!limit.remaining) {
-        return ctx.status = 429; // too many requests
-      } else {
-        try {
-          await next();
-        } catch (err) {
-          const limit = await rateLimiter.get({ id: ip, max: 30 }); // set lower limit
-
-          if (!ctx.response.headerSent) {
-            ctx.response.set('X-Rate-Limit-Limit', limit.total);
-            ctx.response.set('X-Rate-Limit-Remaining', Math.max(0, limit.remaining - 1));
-            ctx.response.set('X-Rate-Limit-Reset', limit.reset);
-          }
-          
-          throw err;
-        }
-      }
-    });
-    
-    webApp.use(async (ctx, next) => {
-      if(!ctx.request.origin === origin) {
-        ctx.app.emit(
-          "info", `[note] [incorrect origin request] ${ctx.format(ctx)}`
-        );
-        return ctx.throw("Not Found", 404);
-      }
-      if(!ctx.request.path.startsWith("/notify/keine/")) {
-        return ctx.throw("Not Found", 404);
-      }
-      return next();
-    });
-
-    webApp.use(async (ctx, next) => {
-      if(ctx.request.path === "/notify/keine/tokens.json") {
-        ctx.response.set("Cache-Control", "no-cache");
-        switch (ctx.request.method) {
-          case "HEAD":
-          case "GET":
-            ctx.status = 200;
-            const accept = ctx.request.accepts("json", "text/plain");
-            if(!accept) {
-              return ctx.status = 406;
-            }
-            if(ctx.request.method === "GET") {
-              ctx.app.emit(
-                "info", `[access] [generate token] ${ctx.format(ctx)}`
-              );
-              return ctx.body = JSON.stringify(await generateTokens(), null, 4);
-            } else {
-              return ;
-            }
-          case "OPTIONS":
-            ctx.status = 204;
-            ctx.response.set(
-              "Access-Control-Allow-Origin", origin
-            );
-            ctx.response.set("Vary", "Origin");
-            return ctx.response.set("Allow", "OPTIONS, GET, HEAD");
-          default:
-            ctx.response.set("Cache-Control", "max-age=604800");
-            return ctx.status = 405;
-        }
-      }
-      return next();
-    });
-
-    const tokensMap = new Map();
-    const authenticator = new Authenticator(tokensMap);
-
-    webApp.use(authenticator.middleware);
-
-    webApp.listen(env["STALKER_NOTIFY_PORT"], "localhost", function () {
-      inputAgent.info(
-        `Notification receiver is running at http://localhost:${this.address().port}`,
-        "green"
-      );
-    });
-  }
   
   inputAgent.use(async (ctx, next) => {
     const data = ctx.input.trim();
